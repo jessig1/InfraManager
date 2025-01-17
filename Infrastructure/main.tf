27ebb3568696637f54d4d6d89235e517fe53f1ef
Jakob Essig <essigj88@outlook.com>
2025-01-17 19:55:32 +0000

    fix

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -9,7 +9,7 @@ resource "azurerm_eventhub_namespace" "consumer_eventhub_namespace" {
   resource_group_name = azurerm_resource_group.consumer_rg.name
   sku                 = "Standard"
   capacity            = 1
-  minimum_tls_version = 1.2
+  minimum_tls_version = "1.2"
 }
 
 resource "azurerm_eventhub" "consumer" {
