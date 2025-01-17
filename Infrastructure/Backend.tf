dfbf74fd03ec9336aa01319337be952e766b2cab
Jakob Essig <essigj88@outlook.com>
2025-01-17 13:30:19 -0500

    Update Backend.tf

--- Infrastructure/Backend.tf
+++ Infrastructure/Backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "tfstate"
     storage_account_name = "jwetfstatesa"                 
     container_name       = "tfstate"                    
-    key                  = "initial.tfstate"    
+    key                  = "infra.tfstate"    
     subscription_id = "b3c09163-31cd-49b9-af36-e9306dd2c06e"   
   }
 }
