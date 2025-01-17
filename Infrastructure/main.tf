aea1a68a1a3d95f558074a6a96d50cbe894a3ff3
Jakob Essig <essigj88@outlook.com>
2025-01-17 20:35:25 +0000

    add db

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -19,3 +19,13 @@ resource "azurerm_eventhub" "consumer" {
   partition_count     = 1
   message_retention   = 1
 }
+
+resource "azurerm_cosmosdb_postgresql_cluster" "infradb" {
+  name                            = "infra-db"
+  resource_group_name             = azurerm_resource_group.consumer_rg.name
+  location                        = azurerm_resource_group.consumer_rg.location
+  administrator_login_password    = "Mytestdb"
+  coordinator_storage_quota_in_mb = 32768
+  coordinator_vcore_count         = 1
+  node_count                      = 0
+}
