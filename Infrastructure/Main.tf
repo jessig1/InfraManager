d7d9f2f11360325865b9510527bc81007f72418f
Jakob Essig <essigj88@outlook.com>
2025-01-17 12:06:21 -0500

    Add Terraform

---
+++ Infrastructure/Main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "consumer_rg" {
+  name     = "consumer-rg"
+  location = "East US"
+}
+
+resource "azurerm_eventhub_namespace" "consumer_eventhub_namespace" {
+  name                = "consumerEventHubNamespace"
+  location            = azurerm_resource_group.consumer_rg.location
+  resource_group_name = azurerm_resource_group.consumer_rg.name
+  sku                 = "Standard"
+  capacity            = 1
+}
+
+resource "azurerm_eventhub" "example" {
+  name              = "consumerEventHub"
+  namespace_name    = azurerm_eventhub_namespace.consumer_eventhub_namespace.name
+  resource_group_name = azurerm_resource_group.consumer_rg.name
+  partition_count   = 1
+  message_retention = 1
+}
